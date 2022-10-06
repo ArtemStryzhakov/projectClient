8b2e1f9cc033f50870900263e8692398321cbbd3
opilane <dhd@gmail.com>
2022-10-06 13:00:20 +0300

    Valmis

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -44,12 +44,13 @@
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
-
+<body>
 <h1>Great success!</h1>
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Testime Sapper!</figcaption>
+	<figcaption>Raccoon!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+</body>
